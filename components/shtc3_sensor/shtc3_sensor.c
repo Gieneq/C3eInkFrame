03e75ddb5e5a234a23ae9edf4d6d863c39a0710f
Pyrograf <pyrograf.pl@gmail.com>
2023-12-09 21:25:07 +0100

    Refined SHTC3 component

--- components/shtc3_sensor/shtc3_sensor.c
+++ components/shtc3_sensor/shtc3_sensor.c
@@ -21,10 +21,38 @@
 #define SHTC3_WAKEUP_CMD         0x3517
 #define SHTC3_SLEEP_CMD          0xB098
 
+#define TEMPERATURE_IDX               0
+#define TEMPERATURE_CRC_IDX           2
+#define HUMIDITY_IDX                  3
+#define HUMIDITY_CRC_IDX              5
+
 static const char* TAG = "SHTC3";
 
 static float temperature = 0.0f;
 static float humidity = 0.0f;
+
+/* SHTC3 CRC-8 properties */
+#define CRC_POLYNOMIAL      0x31
+#define CRC_INITIAL_VALUE   0xFF
+#define CRC_FINAL_XOR       0x00
+
+
+static uint8_t shtc3_calculate_crc(const uint8_t* data, size_t length) {
+    uint8_t crc = CRC_INITIAL_VALUE;
+
+    for (size_t i = 0; i < length; ++i) {
+        crc ^= data[i];
+        for (int bit = 0; bit < 8; ++bit) {
+            if (crc & 0x80) {
+                crc = (crc << 1) ^ CRC_POLYNOMIAL;
+            } else {
+                crc <<= 1;
+            }
+        }
+    }
+
+    return crc ^ CRC_FINAL_XOR;
+}
 
 esp_err_t shtc3_init() {
 #ifdef SHTC3_DEBUG_LOG
@@ -87,7 +115,7 @@ uint16_t shtc3_read_id() {
         i2c_master_cmd_begin(SHTC3_I2C_PORT_NUM, cmd, portMAX_DELAY);
         i2c_cmd_link_delete(cmd);
     }
-    vTaskDelay(pdMS_TO_TICKS(10));
+    vTaskDelay(1);
 
     {
         i2c_cmd_handle_t cmd = i2c_cmd_link_create();
@@ -98,7 +126,7 @@ uint16_t shtc3_read_id() {
         i2c_master_cmd_begin(SHTC3_I2C_PORT_NUM, cmd, portMAX_DELAY);
         i2c_cmd_link_delete(cmd);
     }
-    vTaskDelay(pdMS_TO_TICKS(15));
+    vTaskDelay(1);
 
     return ((uint16_t)data[1]) + (((uint16_t)data[0]) << 8);
 }
@@ -107,6 +135,8 @@ void shtc3_refresh() {
     uint8_t data[6];
 
     ESP_LOGI(TAG, "Write attempt...");
+
+    /* Wake up */
     {
         i2c_cmd_handle_t cmd = i2c_cmd_link_create();
         i2c_master_start(cmd);
@@ -119,8 +149,9 @@ void shtc3_refresh() {
         i2c_master_cmd_begin(SHTC3_I2C_PORT_NUM, cmd, portMAX_DELAY);
         i2c_cmd_link_delete(cmd);
     }
-    vTaskDelay(pdMS_TO_TICKS(15));
+    vTaskDelay(1);
     
+    /* Start measurement */
     {
         i2c_cmd_handle_t cmd = i2c_cmd_link_create();
         i2c_master_start(cmd);
@@ -133,8 +164,9 @@ void shtc3_refresh() {
         i2c_master_cmd_begin(SHTC3_I2C_PORT_NUM, cmd, portMAX_DELAY);
         i2c_cmd_link_delete(cmd);
     }
-    vTaskDelay(pdMS_TO_TICKS(20));
+    vTaskDelay(1);
     
+    /* Read measurements */
     {
         i2c_cmd_handle_t cmd = i2c_cmd_link_create();
         i2c_master_start(cmd);
@@ -146,8 +178,9 @@ void shtc3_refresh() {
         i2c_master_cmd_begin(SHTC3_I2C_PORT_NUM, cmd, portMAX_DELAY);
         i2c_cmd_link_delete(cmd);
     }
-    vTaskDelay(pdMS_TO_TICKS(15));
+    vTaskDelay(1);
 
+    /* Sleep */
     {
         i2c_cmd_handle_t cmd = i2c_cmd_link_create();
         i2c_master_start(cmd);
@@ -160,15 +193,24 @@ void shtc3_refresh() {
         i2c_master_cmd_begin(SHTC3_I2C_PORT_NUM, cmd, portMAX_DELAY);
         i2c_cmd_link_delete(cmd);
     }
-    vTaskDelay(pdMS_TO_TICKS(20));
+    
+    /* Check CRCs */
+    const uint8_t temperature_crc = shtc3_calculate_crc(data + TEMPERATURE_IDX, 2);
+    const uint8_t humidity_crc    = shtc3_calculate_crc(data + HUMIDITY_IDX, 2);
 
-    ESP_LOGI(TAG, "Read done! Raw: %02X %02X %02X %02X %02X %02X.", data[0], data[1], data[2], data[3], data[4], data[5]);
+    ESP_LOGI(TAG, "Read done! Raw: %02X %02X %02X %02X %02X %02X, CRCs: %02X, %02X", 
+    data[0], data[1], data[2], data[3], data[4], data[5], temperature_crc, humidity_crc);
 
-    int16_t raw_humidity = (data[3] << 8) | data[4];
-    humidity = 100.0f * ((float)raw_humidity / 65535.0f);
+    /* Update values if CRC match */
+    if(temperature_crc == data[TEMPERATURE_CRC_IDX]) {
+        uint16_t raw_humidity = (data[HUMIDITY_IDX] << 8) | data[HUMIDITY_IDX + 1];
+        humidity = 100.0f * ((float)raw_humidity / 65535.0f);
+    }
 
-    int16_t raw_temperature = (data[0] << 8) | data[1];
-    temperature = -45.0f + 175.0f * ((float)raw_temperature / 65535.0f);
+    if(humidity_crc == data[HUMIDITY_CRC_IDX]) {
+        int16_t raw_temperature = (data[TEMPERATURE_IDX] << 8) | data[TEMPERATURE_IDX + 1];
+        temperature = -45.0f + 175.0f * ((float)raw_temperature / 65535.0f);
+    }
 }
 
 float shtc3_get_temperature() {
